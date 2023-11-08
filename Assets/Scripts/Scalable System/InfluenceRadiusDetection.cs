2bcf4ca9aabaa283b4515a22735e45a54dca548a
JaderGedeon <jadergedeon@gmail.com>
2023-11-08 12:18:38 -0300

    Folders Fix

--- Assets/Scripts/Scalable System/InfluenceRadiusDetection.cs
+++ Assets/Scripts/Scalable System/InfluenceRadiusDetection.cs
@@ -5,6 +5,14 @@ public class InfluenceRadiusDetection : MonoBehaviour
     [SerializeField]
     private ArtifactHandler m_artifactHandler = new();
 
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.P))
+        {
+            m_artifactHandler.ArtifactType = m_artifactHandler.ArtifactType == (ArtifactType)2 ? 0 : m_artifactHandler.ArtifactType + 1;
+        }
+    }
+
     private void OnTriggerEnter(Collider collider)
     {
         if (collider.isTrigger)
