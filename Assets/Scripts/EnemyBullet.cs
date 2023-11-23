96a4283c4843fce4b306df06a9817d6b88cc00ea
Baltinha <63794832+Baltinha@users.noreply.github.com>
2023-11-23 15:57:28 -0300

    inimigo arma modelo e girando.

--- Assets/Scripts/EnemyBullet.cs
+++ Assets/Scripts/EnemyBullet.cs
@@ -8,19 +8,21 @@ public class EnemyBullet : MonoBehaviour
     public static EnemyBullet Instance { get; private set; }
 
     [field: SerializeField] public BulletSide SideBullets { get; private set; }
-    //[SerializeField] private Transform m_bulletPosition;
 
     private float m_timer = 3f;
     private float m_temptime;
     private Transform m_Right;
     private Transform m_Left;
+    private Transform m_model;
     // Start is called before the first frame update
     void Start()
     {
         m_Left = GetComponentInChildren<Transform>().GetChild(0);
         m_Right = GetComponentInChildren<Transform>().GetChild(1);
+        m_model = GetComponentInChildren<Transform>().GetChild(2);
         Instance = this;
         m_temptime = m_timer;
+
     }
 
     // Update is called once per frame
@@ -37,9 +39,11 @@ public class EnemyBullet : MonoBehaviour
                 {
                     case BulletSide.Left:
                         bullet.transform.position = m_Left.position;
+                        m_model.rotation = Quaternion.Euler(0, 180, 0);
                         break;
                     case BulletSide.Right:
                         bullet.transform.position = m_Right.position;
+                        m_model.rotation = Quaternion.Euler(0, 0, 0);
                         break;
                     default:
                         break;
