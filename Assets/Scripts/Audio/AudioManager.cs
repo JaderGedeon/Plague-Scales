74f3786ca6760905a5ae1214196ae38211e61667
Felipe Andrey Miranda Nunes <senacfelipeandrey@gmail.com>
2023-11-30 23:58:41 -0300

    Audio Manager inserido no game

---
+++ Assets/Scripts/Audio/AudioManager.cs
@@ -0,0 +1,37 @@
+using UnityEngine.Audio;
+using UnityEngine;
+using System;
+
+public class AudioManager : MonoBehaviour
+{
+    public Sound[] sounds;
+    void Awake()
+    {
+        DontDestroyOnLoad(gameObject);
+
+        foreach(Sound sound in sounds)
+        {
+            sound.source = gameObject.AddComponent<AudioSource>();
+            sound.source.clip = sound.clip;
+            sound.source.volume = sound.volume;
+            sound.source.pitch = sound.pitch;
+            sound.source.loop = sound.loop;
+        }
+    }
+
+    private void Start()
+    {
+        
+    }
+
+    public void Play (string name)
+    {
+        Sound sound = Array.Find(sounds, sound => sound.name == name);
+        if(sound == null) 
+        {
+            Debug.LogWarning("Sound: " + name + " not found!");
+            return;
+        }
+        sound.source.Play();
+    }
+}
